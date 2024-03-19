140944ff73082f417c482ecc33b0ebfac87aba4d
Aadishx07 <aadishdas7@gmail.com>
2024-03-19 19:29:34 +0530

    add

--- PHP/sop2/vowel.php
+++ PHP/sop2/vowel.php
@@ -1,12 +1,11 @@
 <?php
-if(isset($_POST['submit']))
-$str = strtolower($_POST['str']);
+if (isset($_POST['submit']))
+    $str = strtolower($_POST['str']);
 $vowels = array('a', 'e', 'i', 'o', 'u');
 $len = strlen($str);
 $num = 0;
-for($i=0;$i<$len; $i++){
-if(in_array($str[$i], $vowels))
-$num++;
+for ($i = 0; $i < $len; $i++) {
+    if (in_array($str[$i], $vowels))
+        $num++;
 }
 echo "Number of vowels: $num";
-?>
--- PHP/sop7/admission.php
+++ PHP/sop7/admission.php
@@ -1,16 +1,17 @@
-
 <!DOCTYPE html>
 <html>
+
 <body>
-<h1 align="center">Addmission Form</h1>
-<form method="post">
-<label>Enter Roll No of Student</label>
-<input type="text" name="rollno" id="id_roll"><br><br>
-<label>Enter Name of Student</label>
-<input type="text" name="name" id="id_name"><br><br>
-<input type="submit" name="submit" value="submit" id="submit">
-</form>
+    <h1 align="center">Addmission Form</h1>
+    <form method="post">
+        <label>Enter Roll No of Student</label>
+        <input type="text" name="rollno" id="id_roll"><br><br>
+        <label>Enter Name of Student</label>
+        <input type="text" name="name" id="id_name"><br><br>
+        <input type="submit" name="submit" value="submit" id="submit">
+    </form>
 </body>
+
 </html>
 <?php
 $servername = "pgsql:host=localhost;dbname=student";
